3b1d3deed8e8eadf48df9aa0d48af26e08817c4f
masichiquito <161529766+masichiquito@users.noreply.github.com>
2024-05-10 17:16:56 -0500

    Update Movimiento.cs

--- Assets/JuegoMaxi/Scripts/Movimiento.cs
+++ Assets/JuegoMaxi/Scripts/Movimiento.cs
@@ -14,7 +14,7 @@ public class Movimiento : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.W))
         {
